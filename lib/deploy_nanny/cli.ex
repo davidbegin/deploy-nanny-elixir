7891b42935cfbddf58d1200d92af221bea291535
David Begin <davidmichaelbe@gmail.com>
2015-11-24 21:36:45 -0800

    the start of parsing args

---
+++ lib/deploy_nanny/cli.ex
@@ -0,0 +1,21 @@
+defmodule DeployNanny.CLI do
+
+  def run(argv) do
+    case parse_args(argv) do
+      :help        -> IO.puts "HELP!"
+      :coming_soon -> IO.puts "DEPLOY NANNY ELIXIR COMING SOON"
+    end
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean ],
+                                     aliases:  [ h:    :help    ])
+
+    case parse do
+      { [ help: true ] }
+        -> :help
+      _ -> :coming_soon
+    end
+  end
+
+end
