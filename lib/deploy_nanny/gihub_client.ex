1abe7d7b9bbf0dd10326977f9be236ac09cc1d55
David Begin <davidmichaelbe@gmail.com>
2015-12-05 20:45:25 -0800

    remove duplicate file

--- lib/deploy_nanny/gihub_client.ex
+++
@@ -1,9 +0,0 @@
-defmodule DeployNanny.GithubClient do
-  def connect do
-    # how can I pipe something to function where it is not the first argument
-    client = Tentacat.Client.new
-
-    Tentacat.Users.find "davidbegin", client
-      |> IO.inspect
-  end
-end
